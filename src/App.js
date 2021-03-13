e5369eeb0b88f352aabf814b0caf6693ab9bec5f
nicholas-bd <nicholas.bond.d@gmail.com>
2021-03-13 12:20:30 +0100

    Added new files started new stoa project

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./App.scss";
+import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route path="/" />
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
