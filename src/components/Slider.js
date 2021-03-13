edb006afb556c080bec8c7cd3b1eab42f5a2799b
nicholas-bd <nicholas.bond.d@gmail.com>
2021-03-13 13:07:10 +0100

    Added assets folder and img files, slider component

---
+++ src/components/Slider.js
@@ -0,0 +1,70 @@
+import React, { useState, useEffect } from "react";
+import { FiChevronRight, FiChevronLeft } from "react-icons/fi";
+import { FaQuoteRight } from "react-icons/fa";
+import data from "../data";
+
+const Slider = () => {
+  // eslint-disable-next-line
+  const [views, setViews] = useState(data);
+  const [index, setIndex] = useState(0);
+
+  useEffect(() => {
+    const lastIndex = views.length - 1;
+    if (index < 0) {
+      setIndex(lastIndex);
+    }
+    if (index > lastIndex) {
+      setIndex(0);
+    }
+  }, [index, views]);
+
+  // Auto slider
+  useEffect(() => {
+    let slider = setInterval(() => {
+      setIndex(index + 1);
+    }, 5000);
+    return () => clearInterval(slider);
+  }, [index]);
+
+  return (
+    <section className="section">
+      <div className="title">
+        <h2>
+          <span>/</span>reviews
+        </h2>
+      </div>
+      <div className="section-center">
+        {views.map((view, viewIndex) => {
+          const { id, image, name, title, quote } = view;
+          let position = "nextSlide";
+          if (viewIndex === index) {
+            position = "activeSlide";
+          }
+          if (
+            viewIndex === index - 1 ||
+            (index === 0 && viewIndex === views.length - 1)
+          ) {
+            position = "lastSlide";
+          }
+          return (
+            <article className={position} key={id}>
+              <img src={image} alt={name} className="person-img" />
+              <h4>{name}</h4>
+              <p className="title">{title}</p>
+              <p className="text">{quote}</p>
+              <FaQuoteRight className="icon" />
+            </article>
+          );
+        })}
+        <button className="prev" onClick={() => setIndex(index - 1)}>
+          <FiChevronLeft />
+        </button>
+        <button className="next" onClick={() => setIndex(index + 1)}>
+          <FiChevronRight />
+        </button>
+      </div>
+    </section>
+  );
+};
+
+export default Slider;
