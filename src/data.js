5ab03463b17b8daa52edb1329d64e48202d6269c
nicholas-bd <nicholas.bond.d@gmail.com>
2021-03-13 13:13:27 +0100

    Added styles and data file

---
+++ src/data.js
@@ -0,0 +1,41 @@
+import Img_1 from "./images/athens.jpg";
+import Img_2 from "./images/acropolis.jpg";
+import Img_3 from "./images/greece.jpg";
+import Img_4 from "./images/greece2.jpg";
+
+const views = [
+  {
+    id: 1,
+    image: Img_1,
+    name: "maria ferguson",
+    title: "office manager",
+    quote:
+      "Fingerstache umami squid, kinfolk subway tile selvage tumblr man braid ",
+  },
+  {
+    id: 2,
+    image: Img_2,
+    name: "john doe",
+    title: "regular guy",
+    quote:
+      "Gastropub sustainable tousled prism occupy. Viral XOXO roof party brunch actually,",
+  },
+  {
+    id: 3,
+    image: Img_3,
+    name: "peter smith",
+    title: "product designer",
+    quote:
+      "Drinking vinegar polaroid street art echo park, actually semiotics next",
+  },
+  {
+    id: 4,
+    image: Img_4,
+    name: "susan andersen",
+    title: "the boss",
+    quote:
+      "Marfa af yr 3 wolf moon kogi, readymade distillery asymmetrical seitan",
+  },
+];
+
+export default views;
