b54056aa502b7b4c40f30d3085365919c1a7b06b
nicholas-bd <nicholas.bond.d@gmail.com>
2021-04-13 17:21:53 +0200

    Played around with article and fonts sizing

--- src/pages/About.js
+++ src/pages/About.js
@@ -8,6 +8,53 @@ function About() {
       <div className="about__container">
         <h2 className="about__title">About Us Section</h2>
       </div>
+      <div className="about__column-wrapper">
+        <div className="about__column-box">
+          <h2 className="about__column-title">This is header</h2>
+          <p className="about__column-desc">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Alias
+            quidem dicta provident esse. Optio odit, harum beatae ipsa nisi
+            deleniti quis repellendus. Quae, unde. In ab blanditiis, alias,
+            atque nulla tempora ratione perspiciatis et qui reprehenderit porro
+            suscipit, earum quisquam molestiae? Dicta blanditiis earum delectus
+            fugit laudantium? Eaque, nobis? Vero provident laboriosam
+            consectetur iusto consequatur quisquam modi optio eligendi placeat?
+            Quas iure ratione repellendus voluptatibus illum officiis
+            repudiandae nam sint delectus velit, maiores, aspernatur corporis
+            perspiciatis architecto et! Ipsam, sit!
+          </p>
+        </div>
+        <div className="about__column-box">
+          <h2 className="about__column-title">This is header</h2>
+          <p className="about__column-desc">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Alias
+            quidem dicta provident esse. Optio odit, harum beatae ipsa nisi
+            deleniti quis repellendus. Quae, unde. In ab blanditiis, alias,
+            atque nulla tempora ratione perspiciatis et qui reprehenderit porro
+            suscipit, earum quisquam molestiae? Dicta blanditiis earum delectus
+            fugit laudantium? Eaque, nobis? Vero provident laboriosam
+            consectetur iusto consequatur quisquam modi optio eligendi placeat?
+            Quas iure ratione repellendus voluptatibus illum officiis
+            repudiandae nam sint delectus velit, maiores, aspernatur corporis
+            perspiciatis architecto et! Ipsam, sit!
+          </p>
+        </div>
+        <div className="about__column-box">
+          <h2 className="about__column-title">This is header</h2>
+          <p className="about__column-desc">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Alias
+            quidem dicta provident esse. Optio odit, harum beatae ipsa nisi
+            deleniti quis repellendus. Quae, unde. In ab blanditiis, alias,
+            atque nulla tempora ratione perspiciatis et qui reprehenderit porro
+            suscipit, earum quisquam molestiae? Dicta blanditiis earum delectus
+            fugit laudantium? Eaque, nobis? Vero provident laboriosam
+            consectetur iusto consequatur quisquam modi optio eligendi placeat?
+            Quas iure ratione repellendus voluptatibus illum officiis
+            repudiandae nam sint delectus velit, maiores, aspernatur corporis
+            perspiciatis architecto et! Ipsam, sit!
+          </p>
+        </div>
+      </div>
     </section>
   );
 }
